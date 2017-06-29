23a6f2691ca2f5a6c6bc0ee1058111eaeed54f68
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2017-06-29 21:50:55 +0100

    21:50 29/06/17

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-
-        String input = "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2";
+//        "L1, L3, L5, L3, R1, L4, L5, L4, R6"; //
+        String input = "L1, L3, L5, L3, R1, L4, L5, L4, R6"; // "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2";
 
         String[] inputArr = input.split(", ");
         ArrayList<String[]> directions = new ArrayList<>();
@@ -18,36 +18,71 @@ public class Main {
             directions.add(dir.split("", 2));
         }
 
-        int[] position = {0,0};
+        int[] pos = {0,0};
+        int[] prevPos = new int[]{};
         Orientation ori = Orientation.NORTH;
+        ArrayList<int[]> history = new ArrayList<>();
+        history.add(pos);
+        int i = 0;
 
+
+        outerLoop:
         for (String[] dir : directions) {
+
+            for (int[] item : history) {
+                if (Arrays.equals(item, pos)) { //stop going through directions if position is in history
+                    break outerLoop;
+                }
+            }
+
+
             if (dir[0].equals("L")) {
                 ori = Orientation.fromNumber((ori.value - 1 + 4) % 4); //extra 4 added to avoid -ve
             } else {
                 ori = Orientation.fromNumber((ori.value + 1) % 4);
             }
 
+            System.arraycopy(pos, 0, prevPos, 0, 2);
+
             int distance = Integer.parseInt(dir[1]);
+
 
             switch (ori) {
                 case NORTH:
-                    position[1] += distance;
+                    for (int j = prevPos[1] + 1 ; j <= pos[1] ; j++ ) {
+                        int[] pastPos = new int[]{pos[0], j};
+                        history.add(pastPos);
+                    }
+                    pos[1] += distance;
                     break;
                 case EAST:
-                    position[0] += distance;
+                    for (int j = prevPos[0] + 1 ; j <= pos[0] ; j++ ) {
+                        int[] pastPos = new int[]{j, pos[1]};
+                        history.add(pastPos);
+                    }
+                    pos[0] += distance;
                     break;
                 case SOUTH:
-                    position[1] -= distance;
+                    for (int j = prevPos[1] - 1 ; j >= pos[1] ; j-- ) {
+                        int[] pastPos = new int[]{pos[0], j};
+                        history.add(pastPos);
+                    }
+                    pos[1] -= distance;
                     break;
                 case WEST:
-                    position[0] -= distance;
+                    for (int j = prevPos[0] - 1 ; j >= pos[0] ; j-- ) {
+                        int[] pastPos = new int[]{j, pos[1]};
+                        history.add(pastPos);
+                    }
+                    pos[0] -= distance;
                     break;
             }
-        }
 
-        System.out.println("I end up at position " + Arrays.toString(position) + " relative to my starting point, facing " + ori + ".");
-        int minDistance = Math.abs(position[0]) + Math.abs(position[1]);
+            i++;
+        }
+        System.out.println("i: " + i);
+        System.out.println("I end up at position " + Arrays.toString(pos) + " relative to my starting point, facing " + ori + ".");
+        int minDistance = Math.abs(pos[0]) + Math.abs(pos[1]);
         System.out.println("So HQ is " + minDistance + " blocks away.");
 
 
