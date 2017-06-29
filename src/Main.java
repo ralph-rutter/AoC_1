a25d3c872c74271f79350856b0f83fc6180c3d4c
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2017-06-29 17:32:58 +0100

    Complete day 1 part 1

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        String input = "L1, L3, L5, L3, R145, L4, L5, R1, R3";//"L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2";
+        String input = "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2";
 
         String[] inputArr = input.split(", ");
         ArrayList<String[]> directions = new ArrayList<>();
@@ -18,26 +18,40 @@ public class Main {
             directions.add(dir.split("", 2));
         }
 
-        System.out.println(Arrays.toString(inputArr));
-
-        for (String[] pair : directions) {
-            System.out.println(Arrays.toString(pair));
-        }
-
         int[] position = {0,0};
         Orientation ori = Orientation.NORTH;
-
-
 
         for (String[] dir : directions) {
             if (dir[0].equals("L")) {
                 ori = Orientation.fromNumber((ori.value - 1 + 4) % 4); //extra 4 added to avoid -ve
             } else {
-                ori = Orientation.fromNumber((ori.value + 1) % 4);            }
+                ori = Orientation.fromNumber((ori.value + 1) % 4);
+            }
+
+            int distance = Integer.parseInt(dir[1]);
+
+            switch (ori) {
+                case NORTH:
+                    position[1] += distance;
+                    break;
+                case EAST:
+                    position[0] += distance;
+                    break;
+                case SOUTH:
+                    position[1] -= distance;
+                    break;
+                case WEST:
+                    position[0] -= distance;
+                    break;
+            }
+//            System.out.println(Arrays.toString(position));
+//            System.out.println(ori);
         }
 
-        System.out.println(ori.value);
-        System.out.println(ori);
+        System.out.println("I end up at position " + Arrays.toString(position) + " relative to my starting point, facing " + ori + ".");
+        int minDistance = Math.abs(position[0]) + Math.abs(position[1]);
+        System.out.println("So HQ is " + minDistance + " blocks away.");
+
 
     }
 
